6172f94ca91301a28a2af2d4cb1dd94abb4010a5
afg572 <afg@cesar.school>
2025-09-30 00:59:27 -0300

    Adicionando os arquivos do Spring Boot

---
+++ backend/src/main/java/com/gestaoviva/Gestao_Viva/GestaoVivaApplication.java
@@ -0,0 +1,13 @@
+package com.gestaoviva.Gestao_Viva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoVivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoVivaApplication.class, args);
+	}
+
+}
