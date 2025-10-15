7b915e945d17da765aa8a946ee4d785a038e3ea2
Duda Rodrigues <147636488+Duda-Rodrigues-Contato@users.noreply.github.com>
2025-10-14 21:34:32 -0300

    Add o nome da tabela no arquivo "Avaliacao.Java" do model.

--- src/main/java/com/example/Gestao_Viva/model/Avaliacao.java
+++ src/main/java/com/example/Gestao_Viva/model/Avaliacao.java
@@ -1,5 +1,10 @@
 package com.example.Gestao_Viva.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "avaliacao_parque")
 public class Avaliacao {
 
 }
