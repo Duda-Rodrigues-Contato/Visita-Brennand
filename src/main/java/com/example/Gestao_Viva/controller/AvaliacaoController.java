124f2a33d63082676f67492565b8eb3ad67dece6
Duda-Rodrigues-Contato <mdudarodrigues.contato@gmail.com>
2025-10-22 19:09:06 -0300

    A página de agradecimento após avaliação tá funcionando de acordo com os id's!!!

--- src/main/java/com/example/Gestao_Viva/controller/AvaliacaoController.java
+++ src/main/java/com/example/Gestao_Viva/controller/AvaliacaoController.java
@@ -39,7 +39,7 @@ public class AvaliacaoController {
             avaliacaoService.salvar(avaliacaoDTO);
         } catch (RuntimeException e) {
             model.addAttribute("errorMessage", e.getMessage());
-            return "avaliacao";
+            return "avaliacao-avaliacao";
         }
 
         return "redirect:/avaliacao-agradecimento";
